ba77ffe5d4fe2de9420cc1c352c0ffae3c67bff3
keple <keplerde@gmail.com>
2023-03-27 21:35:44 +0200

    implementing the_open_link_button

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import time
-
+import webbrowser
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.core.clipboard import Clipboard
 from filesharer import FileSharer
 
 Builder.load_file('frontend.kv')
 
 
 class CameraScreen(Screen):
-
     def start(self):
         """Starts camera and changes button text"""
         self.ids.camera.play = True  # root ссылается на текущий обьект экземпляра
@@ -34,15 +34,29 @@ class CameraScreen(Screen):
 
 
 class ImageScreen(Screen):
+    link_message = "Create a Link First"
     def create_link(self):
         """Accesses the photo filepath, uploads it to the web,
         and inserts the link in the label widget"""
         file_path = App.get_running_app().root.ids.camera_screen.filepath  # нужно получить доступ к скрину из верхнего класса
         filesharer = FileSharer(filepath=file_path)
-        url = filesharer.share()
-        self.ids.link.text = url
+        self.url = filesharer.share()   # добавляю указатель чтобы в следующем методе был доступ к ссылке
+        self.ids.link.text = self.url
+
+    def copy_link(self):
+        """Copy link to the clipboard available for pasting"""
+        try:
+            Clipboard.copy(self.url)  # copy the link in clipboard
+        except:
+            self.ids.link.text = self.link_message
 
 
+    def open_link(self):
+        """Open link with default browser"""
+        try:
+            webbrowser.open(self.url)
+        except:
+            self.ids.link.text = self.link_message
 class RootWidget(ScreenManager):
     pass
 
